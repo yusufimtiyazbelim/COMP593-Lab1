c6a000465cbf424d1a8a65228e166ee5f010617f
yusufimtiyazbelim <133910585+yusufimtiyazbelim@users.noreply.github.com>
2023-05-17 15:41:56 -0400

    Put comment back in script2.py
    Comment was unintentionally deleted by revert.

--- script2.py
+++ script2.py
@@ -1,2 +1,3 @@
+# Print lines from Zen of Python 
 print("Although that way may not be obvious at first unless you're Dutch.")
 print('If the implementation is easy to explain, it may be a good idea.')
