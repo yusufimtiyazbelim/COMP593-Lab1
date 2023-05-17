32ab2d350e4d4dc9d4c4f3ef65c7fc3c14f87e30
yusufimtiyazbelim <133910585+yusufimtiyazbelim@users.noreply.github.com>
2023-05-17 15:54:16 -0400

    Modified both files in the repo clone
    - Deleted a print() statement from script1.py
    - Deleted a print() statement from script2.py

--- script1.py
+++ script1.py
@@ -1,3 +1,2 @@
 # Print lines from Zen of Python
 print("In the face of ambiguity, refuse the temptation to guess.")
-print("Errors should never pass silently.")
--- script2.py
+++ script2.py
@@ -1,3 +1,4 @@
 # Print lines from Zen of Python 
 print("Although that way may not be obvious at first unless you're Dutch.")
 print('If the implementation is easy to explain, it may be a good idea.')
+print("Errors should never pass silently.")
